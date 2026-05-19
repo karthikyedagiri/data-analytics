ec411ff488792a12b2953eff5746c0da89ec2b6c
karthikyedagiri <karthikyedagiri@gmail.com>
2026-05-19 15:02:07 +0530

    type conversions

---
+++ day6.py
@@ -0,0 +1,59 @@
+'''
+type conversion
+------------it converts integers into strings.
+int--->
+'''
+an=89
+any=str(an)
+print(type(any))
+'''
+str--->
+'''
+an="90"
+a=int(an)
+print(type(a))
+'''
+float()---
+'''
+car=90.78
+print(int(car))
+print(type(str(car)))
+'''
+'''
+a=[7,8]
+print(str(a))
+print(tuple(a))
+'''
+tuple--->
+'''
+A=(45,67)
+print(list(A))
+'''
+int as a userinput:
+'''
+atm=int(input("add the amount:"))
+print(50+atm)
+'''
+string as a userinput:
+'''
+hii=input("enter the name:")
+print(hii)
+'''
+'''
+a=tuple(map(int,input("enter the numbers:").split()))
+print(a)
+'''
+'''
+a=list(map(int,input("enter the numbers:").split()))
+print(a)
+'''
+'''
+n=eval(input("enter:"))
+print(n)
+
+
+
+
+
+
+
